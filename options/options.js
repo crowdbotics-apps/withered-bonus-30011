7197c6113e2884135c73d2dd0f35028d2a2b5f83
Crowdbotics <team@crowdbotics.com>
2021-08-21 20:34:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "withered_bonus_30011Identifier",
+  url: "https://withered_bonus_30011Identifier.botics.co",
+  api: "https://withered_bonus_30011Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
